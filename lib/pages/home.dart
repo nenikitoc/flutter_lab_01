89155d80b2ae3985aa4840047860475cc1ad3a6b
nenikitoc <nenikitoc@gmail.com>
2025-12-12 21:59:39 +0300

    second commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,127 +7,181 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 20, 27, 45),
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 20, 27, 45),
-        title: Text(
-          'Wheel of Fortune',
-          style: TextStyle(color: Colors.white, fontSize: 22),
-        ),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {},
-          icon: Image.asset('assets/crown_icon.png', width: 28, height: 28),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Image.asset('assets/gear_icon.png', width: 28, height: 28),
-          ),
-        ],
-      ),
       body: Column(
         children: [
-          Center(
-            child: Text(
-              "Congratz!",
-              style: TextStyle(color: Colors.white, fontSize: 24),
-            ),
-          ),
-          Center(
-            child: Text(
-              "You were got new date idea!",
-              style: TextStyle(color: Colors.blueGrey, fontSize: 16),
-            ),
-          ),
-          SizedBox(height: 160),
-          Center(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(14),
-              child: Image.asset(
-                'assets/main_image.png',
-                fit: BoxFit.fitWidth,
-                width: 360,
-                height: 140,
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          Center(
-            child: Container(
-              width: 360,
-              height: 140,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 33, 30, 75),
-                borderRadius: BorderRadius.circular(14),
-              ),
-              padding: EdgeInsets.all(24),
-              child: Column(
-                children: [
-                  Text(
-                    "A meeting on the hill",
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: const Color.fromARGB(255, 130, 76, 251),
-                    ),
-                  ),
-                  Text(
-                    textAlign: TextAlign.center,
-                    "Under the stars, with herbal tea\nand the songs of the wind",
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: const Color.fromARGB(255, 130, 76, 251),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
           SizedBox(height: 40),
-          SizedBox(
-            width: 360,
-            height: 48,
-            child: FilledButton(
-              style: FilledButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 45, 149, 247),
-              ),
-              onPressed: () {},
-              child: Text(
-                "Add to plan",
-                style: TextStyle(color: Colors.white, fontSize: 18),
-              ),
+          Container(
+            margin: EdgeInsets.all(12),
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(),
+            child: Row(
+              children: [
+                Text(
+                  "Your dream travels\nin one place",
+                  style: TextStyle(color: Colors.white, fontSize: 32),
+                ),
+                SizedBox(width: 12),
+                Image.asset("assets/black_box.png", width: 80, height: 80),
+              ],
             ),
           ),
-          SizedBox(height: 12),
-          SizedBox(
-            width: 360,
+          Container(
+            margin: EdgeInsets.all(12),
+            padding: EdgeInsets.all(2),
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(12),
+            ),
             height: 48,
+            child: Row(
+              children: [
+                Expanded(
+                  child: FilledButton(
+                    style: FilledButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      backgroundColor: Colors.grey,
+                      foregroundColor: Colors.black,
+                    ),
+                    onPressed: () {},
+                    child: Text("Trips", style: TextStyle(fontSize: 16)),
+                  ),
+                ),
+                SizedBox(width: 8),
+                Expanded(
+                  child: FilledButton(
+                    style: FilledButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                    ),
+                    onPressed: () {},
+                    child: Text("Memories", style: TextStyle(fontSize: 16)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(12),
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    'assets/black_box.png',
+                    width: 86,
+                    height: 86,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "My first travel with my\nwife and kids",
+                      style: TextStyle(color: Colors.black, fontSize: 24),
+                    ),
+                    Text(
+                      "08.08.2023 - 30.08.2023",
+                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(12),
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    'assets/black_box.png',
+                    width: 86,
+                    height: 86,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "My second travel with\nmy wife and kids",
+                      style: TextStyle(color: Colors.black, fontSize: 24),
+                    ),
+                    Text(
+                      "08.08.2023 - 30.08.2023",
+                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 220),
+          Container(
+            margin: EdgeInsets.all(8),
+            padding: EdgeInsets.all(8),
+            width: double.infinity,
+            height: 60,
             child: FilledButton(
               style: FilledButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 76, 131, 231),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                backgroundColor: const Color.fromARGB(255, 45, 149, 247),
+                foregroundColor: Colors.white,
               ),
               onPressed: () {},
-              child: Text(
-                "Generate more",
-                style: TextStyle(color: Colors.white, fontSize: 18),
-              ),
+              child: Text("Add memories", style: TextStyle(fontSize: 18)),
             ),
           ),
         ],
       ),
-      bottomNavigationBar: ClipRRect(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(60)),
-        child: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              IconButton(icon: Icon(Icons.home), onPressed: () {}),
-              IconButton(icon: Icon(Icons.search), onPressed: () {}),
-              IconButton(icon: Icon(Icons.person), onPressed: () {}),
-              IconButton(icon: Icon(Icons.search), onPressed: () {}),
-              IconButton(icon: Icon(Icons.person), onPressed: () {}),
-            ],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset("assets/black_box.png", width: 30, height: 30),
+            label: '',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Image.asset("assets/black_box.png", width: 30, height: 30),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset("assets/black_box.png", width: 30, height: 30),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset("assets/black_box.png", width: 30, height: 30),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset("assets/black_box.png", width: 30, height: 30),
+            label: 'Home',
+          ),
+        ],
+        currentIndex: 0,
+        onTap: (index) {},
+        selectedItemColor: Colors.grey,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
       ),
     );
   }
